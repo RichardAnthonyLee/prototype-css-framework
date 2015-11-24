058f0986366eeef1734a12431fbfe579bd20064e
Richard Lee <get@coding-stuff.com>
2015-11-24 16:24:17 -0500

    added new components

--- app/config.rb
+++ app/config.rb
@@ -4,7 +4,7 @@ require 'prototype-css'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "compliled"
+css_dir = "compiled"
 sass_dir = "."
 images_dir = "images"
 javascripts_dir = "javascripts"
